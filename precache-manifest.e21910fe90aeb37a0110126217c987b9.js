0f359d5d59087e583b48eac481c3498e583bc264
ulyavrubel <uvrubel@gmail.com>
2020-01-23 17:33:22 +0800

    Updates

---
+++ precache-manifest.e21910fe90aeb37a0110126217c987b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0213284fb58272085a30158b47fb2ef",
+    "url": "/quoteMachine/index.html"
+  },
+  {
+    "revision": "8fed5233f2da039c72ad",
+    "url": "/quoteMachine/static/css/main.42f5ff94.chunk.css"
+  },
+  {
+    "revision": "599a307b2e766b6422a6",
+    "url": "/quoteMachine/static/js/2.46130820.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quoteMachine/static/js/2.46130820.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8fed5233f2da039c72ad",
+    "url": "/quoteMachine/static/js/main.5de14bcd.chunk.js"
+  },
+  {
+    "revision": "62a97a3121fbf9ef8868",
+    "url": "/quoteMachine/static/js/runtime-main.aa2e5bae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quoteMachine/static/media/logo.5d5d9eef.svg"
+  }
+]);
